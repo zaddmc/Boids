49638fd4ef413c2a3b305aa885e364beb1fac1b9
Asbjørn Juhl Holm Pedersen <asbj1702@elev.heguddannelser.dk>
2024-03-18 13:54:24 +0100

    test.

--- Overlord.cs
+++ Overlord.cs
@@ -6,7 +6,8 @@ public partial class Overlord : Node2D
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-	}
+	int ef 34;		
+	} 
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
