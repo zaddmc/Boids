b40ba9b568c85356229c3d6b857bea6070d7b581
Zadd ZC <davidbk927@gmail.com>
2024-03-18 13:55:48 +0100

    yoo

--- Overlord.cs
+++ Overlord.cs
@@ -6,7 +6,7 @@ public partial class Overlord : Node2D
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-	int ef 34;		
+	int ef = 34;		
 	} 
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
