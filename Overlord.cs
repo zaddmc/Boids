f484da9fbe79c5f7ca59264524beec8cf7c60fb9
Zadd ZC <davidbk927@gmail.com>
2024-03-18 15:52:32 +0100

    give up for the day

--- Overlord.cs
+++ Overlord.cs
@@ -3,11 +3,13 @@ using System;
 using System.Collections.Generic;
 
 public partial class Overlord : Node2D {
+    [Export]
+    public int SpriteCount = 10;
     // Called when the node enters the scene tree for the first time.
     public override void _Ready() {
         Sprites = new List<Sprite2D>();
 
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < SpriteCount; i++) {
             Sprite2D sprite2D = new Sprite2D();
             sprite2D.Texture = (Texture2D)GD.Load("res://icon.svg");
             //base._Ready();
@@ -15,8 +17,17 @@ public partial class Overlord : Node2D {
             Random random = new Random();
             sprite2D.Position = new Vector2(random.NextSingle() * 1000, random.NextSingle() * 500);
 
+            RigidBody2D rigidBody = new RigidBody2D();
+            rigidBody.AngularVelocity = 1;
+            rigidBody.Rotation = random.NextSingle() * 2;
+            rigidBody.GravityScale = 0;
+
+            sprite2D.AddChild(rigidBody);
+            
+
             Sprites.Add(sprite2D);
             node.AddChild(sprite2D);
+
 
         }
 
@@ -25,5 +36,6 @@ public partial class Overlord : Node2D {
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
     public override void _Process(double delta) {
+        
     }
 }
