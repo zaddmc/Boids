4ca90ae29fb330045ad4a3ba01ed7aef20510ddf
Asbjørn Juhl Holm Pedersen <asbj1702@elev.heguddannelser.dk>
2024-03-18 15:53:23 +0100

    setting up things. rough beginnings. made the radius range.

--- Overlord.cs
+++ Overlord.cs
@@ -26,4 +26,35 @@ public partial class Overlord : Node2D {
     // Called every frame. 'delta' is the elapsed time since the previous frame.
     public override void _Process(double delta) {
     }
+    public void Alignment() {//find hvilke boids der er i radius. if is in range then. så gem deres alignment/retning i xy og antal. bagefter divider med x og y med antal. 
+
+        float radius = 100;
+        for (int i = 0; i < Sprites.Count; i++) {
+            float newalignment;
+            float xLowRange;
+            float xHighRange;
+            float yLowRange;
+            float yHighRange;
+
+            xLowRange = Sprites[0].Position.X - radius;
+            xHighRange = Sprites[0].Position.X - radius;
+            yLowRange = Sprites[0].Position.Y - radius;
+            yHighRange = Sprites[0].Position.Y - radius;
+
+            for (int j = 0; j < Sprites.Count; j++) {
+
+            }
+            if (xLowRange < 0) { }
+
+            //Sprites[0].Position = Sprites[1].Position;
+
+        }
+    }
+    public bool IsInRange() {
+
+        return true;
+    }
+    public void CalcAlignment() {
+
+    }
 }
